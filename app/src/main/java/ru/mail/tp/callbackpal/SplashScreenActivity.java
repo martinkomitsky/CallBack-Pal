40020898d6b3c8c146395907746c29afff74ba87
Martin Komitsky <martin00@yandex.ru>
2017-02-05 23:05:47 +0300

    removed hard link and weak reference

--- app/src/main/java/ru/mail/tp/callbackpal/SplashScreenActivity.java
+++ app/src/main/java/ru/mail/tp/callbackpal/SplashScreenActivity.java
@@ -5,83 +5,53 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import java.lang.ref.WeakReference;
 
 public class SplashScreenActivity extends AppCompatActivity {
-	static boolean backPressed = false;
-
-	public static Boolean isValidated;
+	public boolean isValidated;
+	private boolean backPressed;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
-		getSupportActionBar().hide();
 		backPressed = false;
+		ActionBar supportActionBar = getSupportActionBar();
+
+		if (supportActionBar != null) {
+			supportActionBar.hide();
+		}
 
 		SharedPreferences pref = getApplicationContext().getSharedPreferences("ValidationData", MODE_PRIVATE);
-
 		isValidated = pref.getBoolean("phone_validated", false);
 
-		ActivityStarter activityStarter = new ActivityStarter(this);
-		ThreadSleep threadSleep = new ThreadSleep(activityStarter);
-		threadSleep.start();
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				if (!backPressed) {
+					if (isValidated) {
+						Log.d("Phone is", "validated");
+						Intent startSecondActivity = new Intent(SplashScreenActivity.this, ContactsListActivity.class);
+						startActivity(startSecondActivity);
+					} else {
+						Log.d("Phone is", "not validated");
+						Intent startSecondActivity = new Intent(SplashScreenActivity.this, LoginActivity.class);
+						startActivity(startSecondActivity);
+					}
+					finish();
+				}
+			}
+		}, 2000);
 	}
 
 	@Override
 	protected void onStop() {
 		backPressed = true;
 		super.onStop();
-	}
-
-	private static class ThreadSleep extends Thread {
-		private ActivityStarter activityStarter;
-
-		@Override
-		public void run() {
-			try {
-				sleep(2000);
-				if (!backPressed) {
-					activityStarter.sendMessage(new Message());
-				}
-
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-
-		private ThreadSleep(ActivityStarter aS) {
-			super();
-			this.activityStarter = aS;
-		}
-	}
-
-	private static class ActivityStarter extends Handler {
-		private WeakReference<SplashScreenActivity> link;
-
-		public ActivityStarter(SplashScreenActivity listener) {
-			this.link = new WeakReference<>(listener);
-		}
-
-		@Override
-		public void handleMessage(Message msg) {
-			SplashScreenActivity hardLink = this.link.get();
-			if (hardLink != null) {
-
-				if (isValidated) {
-					Log.d("Phone is", "validated");
-					Intent startSecondActivity = new Intent(hardLink, ContactsListActivity.class);
-					hardLink.startActivity(startSecondActivity);
-				} else {
-					Log.d("Phone is", "not validated");
-					Intent startSecondActivity = new Intent(hardLink, LoginActivity.class);
-					hardLink.startActivity(startSecondActivity);
-				}
-				hardLink.finish();
-			}
-		}
 	}
 }

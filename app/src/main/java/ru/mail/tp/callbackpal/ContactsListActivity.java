c2d844db36a46e87d50c7f9417523653e228c98d
Martin Komitsky <martin00@yandex.ru>
2017-02-15 11:01:28 +0300

    get phonelist via loaders

--- app/src/main/java/ru/mail/tp/callbackpal/ContactsListActivity.java
+++ app/src/main/java/ru/mail/tp/callbackpal/ContactsListActivity.java
@@ -1,6 +1,9 @@
 package ru.mail.tp.callbackpal;
 
+import android.app.LoaderManager;
 import android.content.ContentResolver;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -18,25 +21,35 @@ import java.util.List;
 import ru.mail.tp.callbackpal.contacts.Contact;
 import ru.mail.tp.callbackpal.contacts.ContactsAdapter;
 
-public class ContactsListActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+public class ContactsListActivity extends AppCompatActivity implements SearchView.OnQueryTextListener, LoaderManager.LoaderCallbacks<Cursor> {
 
 
 	private RecyclerView rvContacts;
 	private ContactsAdapter contactAdapter;
 	private List<Contact> contactList;
 
+
+	private static final String[] PROJECTION = {
+			ContactsContract.Data._ID,
+			ContactsContract.Data.HAS_PHONE_NUMBER,
+			ContactsContract.Data.DISPLAY_NAME
+	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contacts_list);
 
-		List<Contact> contactList = getAllContactsList();
-		this.contactList = contactList;
+//		List<Contact> contactList = getAllContactsList();
 		rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
-		ContactsAdapter contactAdapter = new ContactsAdapter(contactList, getApplicationContext());
-		this.contactAdapter = contactAdapter;
-		rvContacts.setLayoutManager(new LinearLayoutManager(this));
-		rvContacts.setAdapter(contactAdapter);
+		getLoaderManager().initLoader(0, null, this);
+
+		this.contactList = contactList;
+//		rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+//		ContactsAdapter contactAdapter = new ContactsAdapter(contactList, getApplicationContext());
+//		this.contactAdapter = contactAdapter;
+//		rvContacts.setLayoutManager(new LinearLayoutManager(this));
+//		rvContacts.setAdapter(contactAdapter);
 	}
 
 	@Override
@@ -47,69 +60,71 @@ public class ContactsListActivity extends AppCompatActivity implements SearchVie
 		final SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
 		searchView.setOnQueryTextListener(this);
 
-		MenuItemCompat.setOnActionExpandListener(item,
-				new MenuItemCompat.OnActionExpandListener() {
-					@Override
-					public boolean onMenuItemActionCollapse(MenuItem item) {
-						// Do something when collapsed
-						contactAdapter.setFilter(contactList);
-						return true; // Return true to collapse action view
-					}
+		MenuItemCompat.setOnActionExpandListener(
+			item,
+			new MenuItemCompat.OnActionExpandListener() {
+				@Override
+				public boolean onMenuItemActionCollapse(MenuItem item) {
+					// Do something when collapsed
+					contactAdapter.setFilter(contactList);
+					return true; // Return true to collapse action view
+				}
 
-					@Override
-					public boolean onMenuItemActionExpand(MenuItem item) {
-						// Do something when expanded
-						return true; // Return true to expand action view
-					}
-				});
+				@Override
+				public boolean onMenuItemActionExpand(MenuItem item) {
+					// Do something when expanded
+					return true; // Return true to expand action view
+				}
+			}
+		);
 
 		return super.onCreateOptionsMenu(menu);
 	}
 
-	private List<Contact> getAllContactsList() {
-		List<Contact> contactList = new ArrayList<>();
-		Contact contact;
-
-		ContentResolver contentResolver = getContentResolver();
-		if (contentResolver != null) {
-
-			Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
-			if (cursor != null) {
-				if (cursor.getCount() > 0) {
-					while (cursor.moveToNext()) {
-
-						int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
-						if (hasPhoneNumber > 0) {
-							String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-							String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-
-							contact = new Contact();
-							contact.setContactName(name);
-
-							Cursor phoneCursor = contentResolver.query(
-									ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-									null,
-									ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-									new String[]{id},
-									null
-							);
-
-							if (phoneCursor != null) {
-								if (phoneCursor.moveToNext()) {
-									String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-									contact.setContactNumber(phoneNumber);
-								}
-								phoneCursor.close();
-								contactList.add(contact);
-							}
-						}
-					}
-				}
-				cursor.close();
-			}
-		}
-		return contactList;
-	}
+//	private List<Contact> getAllContactsList() {
+//		List<Contact> contactList = new ArrayList<>();
+//		Contact contact;
+//
+//		ContentResolver contentResolver = getContentResolver();
+//		if (contentResolver != null) {
+//
+//			Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+//			if (cursor != null) {
+//				if (cursor.getCount() > 0) {
+//					while (cursor.moveToNext()) {
+//
+//						int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+//						if (hasPhoneNumber > 0) {
+//							String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+//							String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+//
+//							contact = new Contact();
+//							contact.setContactName(name);
+//
+//							Cursor phoneCursor = contentResolver.query(
+//									ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+//									null,
+//									ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+//									new String[]{id},
+//									null
+//							);
+//
+//							if (phoneCursor != null) {
+//								if (phoneCursor.moveToNext()) {
+//									String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+//									contact.setContactNumber(phoneNumber);
+//								}
+//								phoneCursor.close();
+//								contactList.add(contact);
+//							}
+//						}
+//					}
+//				}
+//				cursor.close();
+//			}
+//		}
+//		return contactList;
+//	}
 
 	@Override
 	public boolean onQueryTextSubmit(String query) {
@@ -134,5 +149,71 @@ public class ContactsListActivity extends AppCompatActivity implements SearchVie
 			}
 		}
 		return filteredModelList;
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+		return new CursorLoader(
+				this,
+				ContactsContract.Contacts.CONTENT_URI,
+				PROJECTION,
+//				SELECTION,
+//				mSelectionArgs,
+//				null,
+				null,
+				null,
+				ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC"
+		);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		List<Contact> contactList = new ArrayList<>();
+		Contact contact;
+		ContentResolver contentResolver = getContentResolver();
+		if (cursor != null) {
+			if (cursor.getCount() > 0) {
+				while (cursor.moveToNext()) {
+
+					int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+					if (hasPhoneNumber > 0) {
+						String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+						String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+						contact = new Contact();
+						contact.setContactName(name);
+
+						Cursor phoneCursor = contentResolver.query(
+								ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+								null,
+								ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+								new String[]{id},
+								null
+						);
+
+						if (phoneCursor != null) {
+							if (phoneCursor.moveToNext()) {
+								String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+								contact.setContactNumber(phoneNumber);
+							}
+							phoneCursor.close();
+							contactList.add(contact);
+						}
+					}
+				}
+			}
+		}
+
+		this.contactList = contactList;
+
+		ContactsAdapter contactAdapter = new ContactsAdapter(contactList, getApplicationContext());
+		this.contactAdapter = contactAdapter;
+		rvContacts.setLayoutManager(new LinearLayoutManager(this));
+		rvContacts.setAdapter(contactAdapter);
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+
 	}
 }
